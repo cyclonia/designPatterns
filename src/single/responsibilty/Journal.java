2f3d8f89bbfcac19a76036f0d9be35166d3b995e
Sunil Sharma <myworldsunil@gmail.com>
2021-02-16 13:43:44 +0530

    first commit

---
+++ src/single/responsibilty/Journal.java
@@ -0,0 +1,38 @@
+package single.responsibilty;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Journal {
+	
+	private final List<String> entries = new ArrayList<>();
+	private static int count = 0;
+	
+	
+	public void addEntry(String text) {
+		
+		entries.add("" + (count++) + ": " + text);
+	}
+	
+	public void removeEntry(int index) {
+		entries.remove(index);
+	}
+	
+	@Override
+	public String toString() {
+		return String.join(System.lineSeparator(), entries);
+	}
+
+}
+
+class Demo{
+	public static void main(String[] args) {
+		Journal j = new Journal();
+		j.addEntry("I studies today");
+		j.addEntry("I ate a bug");
+		
+		System.out.println(j);
+	}
+}
